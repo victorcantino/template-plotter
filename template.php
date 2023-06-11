91327ec77c710710c79538055dfa010888d7b05b
Victor Hugo Cantino <victor.cantino@gmail.com>
2023-06-11 08:12:16 -0300

    Gerando o arquivo corretamente. O problema era na quebra automática de página

--- template.php
+++ template.php
@@ -1,37 +1,48 @@
 <?php
-
 require_once __DIR__ . '/vendor/autoload.php';
 
+
+// Definir as dimensões do PNG em mm
+$larguraPNG = 20;
+$alturaPNG = 370;
+
 // Definir as dimensões do PDF em mm
-$larguraPDF = 260; // 26cm em mm
-$alturaPDF = 370; // 370mm
+$larguraPDF = 260;
+$alturaPDF = $alturaPNG;
+$fitas = 11; // a quantidade de fitas vai depender da largura da fita
 
 // Criar nova instância do TCPDF
 $pdf = new TCPDF('P', 'mm', array($larguraPDF, $alturaPDF), true, 'UTF-8', false);
 
 // Definir informações do documento PDF
-$pdf->SetCreator('PDF_CREATOR');
-$pdf->SetAuthor('Seu Nome');
+$pdf->SetCreator('TCPDF');
+$pdf->SetAuthor('Victor Cantino');
 $pdf->SetTitle('Exemplo de criação de template PDF com PHP e TCPDF');
 $pdf->SetPrintHeader(false);
 $pdf->SetPrintFooter(false);
+// permite que a imagem toque a borda inferior
+$pdf->setAutoPageBreak(false);
 
 // Adicionar nova página ao PDF
 $pdf->AddPage();
-
-// Definir as dimensões da imagem
-$imagemLargura = 20;
-$imagemAltura = $alturaPDF;
 
 // Carregar a imagem PNG
 $imagePath = __DIR__ . '/imagem.png';
 
 // Distribuir as imagens horizontalmente
-for ($i = 0; $i < 11; $i++) {
-    $x = $i * 23.63;
+for ($i = 0; $i < $fitas; $i++) {
+    $x = $i * ($larguraPDF / $fitas);
     $y = 0;
-    $pdf->Image($imagePath, $x, $y);
+    $pdf->Image($imagePath, $x, $y, 0, $alturaPNG);
 }
+echo $pdf->getImageRBX();
+$pdf->setLineStyle([
+    'dash' => 10,
+    'color' => [255, 0, 0],
+    'width' => 1
+]);
+$pdf->Line(0, 25, 260, 25);
+$pdf->getImageRBX();
 
 // Gerar o arquivo PDF
 $pdf->Output(__DIR__ . '/template.pdf', 'F');
