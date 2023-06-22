ce1e813425f7ff6de27ef5cc95433b42792b9a03
Victor Hugo Cantino <victor.cantino@gmail.com>
2023-06-22 07:24:26 -0300

    Refatorando

--- ProparaImagem.php
+++ ProparaImagem.php
@@ -2,14 +2,16 @@
 
 class PreparaImagem
 {
-
-    function validaImagem($arquivo): bool
+    public function __construct(private string $nome, private string $tipo, private string $nome_temp)
+    {
+    }
+    function validaImagem(): bool
     {
         return filter_var($arquivo['type'], FILTER_SANITIZE_FULL_SPECIAL_CHARS) !== false && $arquivo['type'] == 'image/png';
     }
 
     // Salva um arquivo com o 
-    function salvaImagem($arquivo, $nome)
+    function salvaImagem()
     {
         if (!move_uploaded_file($arquivo['tmp_name'], __DIR__ . "/$nome.png")) {
             echo 'Erro ao salvar o arquivo: ' . __DIR__ . "/$nome.png";
@@ -17,7 +19,7 @@ class PreparaImagem
         }
     }
 
-    function salvaImagens($arquivos, $nome)
+    function salvaImagens($arquivos)
     {
         foreach (IMAGENS as $imagem) {
             if (validaImagem($arquivos[$imagem])) {
@@ -26,4 +28,12 @@ class PreparaImagem
         }
     }
 
+    function verticalizar(): void
+    {
+        // Carregar a imagem usando a biblioteca GD
+        $imagem = imagecreatefrompng($imagemPath);
+
+        // Girar a imagem verticalmente (espelhar)
+        $imagemVertical = imageflip($imagem, IMG_FLIP_VERTICAL);
+    }
 }
